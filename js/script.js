d969fe0d6b273d57f513bfbe763fa7646aed0b6e
Suraj Kadian <138515654+SurajKadian@users.noreply.github.com>
2024-04-23 16:44:56 +0530

    Merging Pro branch (#1)
    * Updated index.html
    
    * Delete js/jsdiff-v520.js
    
    * Delete js/script.js
    
    * Add files via upload
    
    * Delete style.css
    
    * Add files via upload
    
    * Rename script.js to js/script.js
    
    * Removed text from text areas index.html
    
    * Increased size of text area style.css
    
    * added new styles to highlight
    
    and removed unnecessary code lines
    
    * improved output script.js
    
    and removed unnecessary code lines
    
    * improved existing code index.html
    
    * increased the textarea size style.css
    
    * Updated timeLeft and provided text in script.js
    
    Their values were reduced when testing
    
    * disabled spellcheck in text1
    
    * changed the body height to 100vh
    
    * minor changes in script.js
    
    1. added body height in rearrange layout 
    2. removed old accuracy function 
    3. some tweaks in output2
    4. added condition to check if submit button is clicked, before edit timer prompt
    
    * Textarea size 18 to 20rem
    
    * bottom margin in body styles
    
    * overscroll behaviour 
    
    disabled scroll down to refresh, to prevent unintended page refresh
    
    * changed theme-color
    
    * minor styles tweak
    
    including background gradient
    
    * Updated blue words output 
    
    now includes green words in list of half mistakes

--- js/script.js
+++ js/script.js
@@ -1,111 +1,84 @@
 var text1 = document.getElementById("text1");
 var text2 = document.getElementById("text2");
-var diffWord = document.getElementById("diff-word");
+var submit = document.getElementById("submit");
+var result =  document.getElementById('result');
 var output = document.getElementById("output");
+var output2 = document.getElementById('detailedOutput');
 var heading = document.querySelector('.container h1');
-//var infoLine = document.querySelector('.container p');
+//var resultsHeading = document.createElement('h2');
 var container = document.querySelector('.container');
-var calButton = document.getElementById('cal');
-var calculationContainer = document.getElementById('calculation-container');
-var calculationOutput = document.getElementById('calculation-output');
-var fullMistakesInput = document.getElementById('full-mistakes');
-var halfMistakesInput = document.getElementById('half-mistakes');
-var totalWordsInput = document.getElementById('total-words');
-var calculateButton = document.getElementById('calculate-errors');
+const timer = document.getElementById('timer');
 let timerInterval;
 let timeLeft = 600;
-var time = "Time left: "
+var time = "Time left: ";
 let timerStarted = false;
+let submitButtonClicked = false;
 let increaseTime = false;
 let currentFontSize = 16;
 const ttBtn = document.getElementById('tt-btn');
 const fullscreenBtn = document.getElementById('fullscreen-btn');
 
 fullscreenBtn.addEventListener('click', () => {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-    } else {
-        document.documentElement.requestFullscreen();
-    }
-}); 
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    document.documentElement.requestFullscreen();
+  }
+});
 
 function startTimer() {
-    if (!timerStarted) {
-        timerInterval = setInterval(updateTimer, 1000);
-        timerStarted = true;
-    }
+  if (!timerStarted && !submitButtonClicked) {
+    timerInterval = setInterval(updateTimer, 1000);
+    timerStarted = true;
+  }
 }
 
 function updateTimer() {
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
-  const timerDisplay = document.getElementById('label');
-  timerDisplay.textContent = `${time} ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  
-  if (timeLeft === 0){ 
-      if (confirm("Time's up! Do you want to submit?") ) {
-        clearInterval(timerInterval);
-        diffWord.click();
-      } else{
-        increaseTime = true;
-        timeLeft=1;
-      }
-  } 
+  const timer = document.getElementById('label');
+  timer.textContent = `${time} ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+  if (timeLeft === 0) {
+    if (confirm("Time's up! Do you want to submit?")) {
+      clearInterval(timerInterval);
+      submit.click();
+    } else {
+      increaseTime = true;
+      timeLeft = 1;
+    }
+  }
   else {
-    if(!increaseTime){
-    timeLeft--;
-      if (timeLeft<240){
-        timerDisplay.style.color='red';
+    if (!increaseTime) {
+      timeLeft--;
+      if (timeLeft < 240) {
+        timer.style.color = 'red';
       }
     } else {
       timeLeft++;
       time = "Time Elapsed: ";
-      timerDisplay.style.color='light green';
-      timerDisplay.style.fontWeight ='semi-bold';
+      timer.style.color = 'green';
+      timer.style.fontWeight = 'semi-bold';
     }
   }
 }
 
 function parseTimeToSeconds(timeString) {
-    const [minutes, seconds] = timeString.split(':').map(parseFloat);
-    return minutes * 60 + seconds;
-}
-
-function countWords(text) {
-  var words = text.split(/\s+/);
-  return words.filter(function(word) {
-    return word.length > 0;
-  }).length;
-}
-
-function calculateAccuracy(originalText, typedText) {
-  const changes = Diff.diffChars(originalText, typedText);
-  let correctCount = 0;
-  let totalCount = 0;
-  changes.forEach(change => {
-    if (change.added || change.removed) {
-      return;
-    }
-    correctCount += change.value.length;
-    totalCount += change.value.length;
-  });
-  const accuracy = (correctCount / originalText.length) * 100;
-  return accuracy.toFixed(2);
+  const [minutes, seconds] = timeString.split(':').map(parseFloat);
+  return minutes * 60 + seconds;
 }
 
 function rearrangeLayout() {
+  document.body.style.height = '200vh';
+  result.style.display = 'block';
   output.style.display = 'block';
+  output2.style.display = 'block';
   heading.textContent = 'Results';
-  text1.style.width = 'calc(100%)';
   text1.style.height = '12rem';
-  //text1.style.marginRight = '2rem';
-  text2.style.width = 'calc(100%)';
   text2.style.height = '12rem';
-  //text2.style.marginLeft = '2rem';
-  document.getElementById('calculator').style.display = 'block';
-  calButton.style.display = 'block';
-  document.getElementById('calculation-container').style.display = 'none';
-  document.getElementById('label').style.display = 'none';
+  clearInterval(timerInterval);
+  
+  //document.getElementById('r-label').style.display = 'none';
 }
 
 function loadTextFile(fileUrl) {
@@ -121,35 +94,14 @@ function loadTextFile(fileUrl) {
     });
 }
 
-document.getElementById('text2').addEventListener('input', startTimer);
+text2.addEventListener('input', startTimer);
 
-diffWord.addEventListener("click", function() {
-  rearrangeLayout();
-  var text1Value = text1.value;
-  var text2Value = text2.value;
-  var charCount1 = text1Value.length;
-  var charCount2 = text2Value.length; 
-  var changes = Diff.diffWords(text2Value, text1Value);
-  var diffOutput = Diff.convertChangesToXML(changes);
-  var countWords1 = countWords(text1Value);
-  var countWords2 = countWords(text2Value);
-  var totalWordsOriginal = Math.round(charCount1 / 5);
-  var totalWordsTyped = Math.round(charCount2 / 5);
-  var accuracy = calculateAccuracy(text1Value, text2Value);
-
-  result.innerHTML = `<b>Total Words</b>: ${countWords1}[~${totalWordsOriginal}]; `;
-  result.innerHTML += `<b>Words typed</b>: ${countWords2}[~${totalWordsTyped}]; `;
-  result.innerHTML += `<b>Accuracy</b>: ${accuracy}%<br><br>`;
-  output.innerHTML = `${diffOutput}`;
-  
-});
-
-document.getElementById('reset-btn').addEventListener('click', function() {
+document.getElementById('reset').addEventListener('click', function() {
   location.reload();
 });
 
 var providedTexts = {
-  '1': 'text/CGL58.txt',
+    '1': 'text/CGL58.txt',
   '2': 'text/CGL61.txt',
   '3': 'text/CGL62.txt',
   '4': 'text/CGL64.txt',
@@ -180,13 +132,12 @@ var providedTexts = {
 };
 
 ttBtn.addEventListener('click', () => {
-    currentFontSize += 2;
-
-    if (currentFontSize > 22) {
-        currentFontSize = 16;
-    }
- document.getElementById('text1').style.fontSize = currentFontSize + 'px';
-    document.getElementById('text2').style.fontSize = currentFontSize + 'px';
+  currentFontSize += 2;
+  if (currentFontSize > 22) {
+    currentFontSize = 16;
+  }
+  text1.style.fontSize = currentFontSize + 'px';
+  text2.style.fontSize = currentFontSize + 'px';
 });
 
 document.getElementById('file-upload-icon').addEventListener('click', function() {
@@ -199,7 +150,7 @@ document.getElementById('file-input').addEventListener('change', function(event)
     const reader = new FileReader();
     reader.onload = function(e) {
       const text = e.target.result;
-      document.getElementById('text1').value = text;
+      text1.value = text;
     };
     reader.readAsText(file);
   }
@@ -209,63 +160,211 @@ document.getElementById('text-selector').addEventListener('change', function() {
   var selectedFileName = this.value;
   var selectedFileUrl = providedTexts[selectedFileName];
   if (selectedFileUrl) {
-    
     loadTextFile(selectedFileUrl)
       .then(content => {
-        // Populate text1 with the content of the text file
-        document.getElementById('text1').value = content;
+        text1.value = content;
       });
   } else {
-    document.getElementById('text1').value = ''; 
+    text1.value = '';
   }
 });
 
-document.getElementById('timer').addEventListener('click', function() {
-    const modal = document.getElementById('myModal');
-    modal.style.display = 'block';
-});
-
-document.getElementById('apply-btn').addEventListener('click', function() {
-    const newTime = document.getElementById('timer-input').value;
-    const timerDisplay = document.getElementById('timer');
-    timerDisplay.textContent = `Time: ${newTime}`;
-    timeLeft = parseTimeToSeconds(newTime);
-    const modal = document.getElementById('myModal');
-    modal.style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    const modal = document.getElementById('myModal');
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-});
-
 document.querySelector('.text-container label').addEventListener('click', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 });
 
 for (var fileName in providedTexts) {
   var option = document.createElement('option');
   option.value = fileName; // Use file name as value
-  option.textContent = providedTexts[fileName].split('/').pop().split('.').slice(0, -1).join('.'); 
+  option.textContent = providedTexts[fileName].split('/').pop().split('.').slice(0, -1).join('.');
   document.getElementById('text-selector').appendChild(option);
 }
 
-calButton.addEventListener('click', function() {
-  calculationContainer.style.display = 'block';
-  calButton.style.display = 'none';
+
+timer.addEventListener('click', function() {
+if (!submitButtonClicked) {
+  const newTime = window.prompt('Enter new time (mm:ss):', '10:00');
+  if (newTime !== null) {
+    const timerDisplay = document.getElementById('timer');
+    timerDisplay.textContent = `Time: ${newTime}`;
+    timeLeft = parseTimeToSeconds(newTime);
+  }
+}
 });
 
-calculateButton.addEventListener('click', function() {
-  var fullMistakes = parseInt(fullMistakesInput.value);
-  var halfMistakes = parseInt(halfMistakesInput.value);
-  var totalWords = parseInt(totalWordsInput.value);
-
+function errorsPercentage(fullMistakes, halfMistakes, totalWords) {
   if (!isNaN(fullMistakes) && !isNaN(halfMistakes) && !isNaN(totalWords)) {
     var errorsPercentage = ((fullMistakes + (halfMistakes / 2)) / totalWords) * 100;
-    calculationOutput.textContent = 'Errors Percentage: ' + errorsPercentage.toFixed(2) + '%';
+    return errorsPercentage.toFixed(2);
   } else {
-    calculationOutput.textContent = 'Invalid input!';
+    return 'Could not calculate Error Percentage!';
   }
+}
+
+function ld(word1, word2) {
+  var m = word1.length;
+  var n = word2.length;
+  var dp = [];
+
+  for (var i = 0; i <= m; i++) {
+    dp[i] = [];
+    for (var j = 0; j <= n; j++) {
+      if (i === 0) {
+        dp[i][j] = j;
+      } else if (j === 0) {
+        dp[i][j] = i;
+      } else if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  var distance = dp[m][n];
+  var maxLength = Math.max(m, n);
+  var similarityPercentage = ((maxLength - distance) / maxLength) * 100;
+
+  return {
+    distance: distance,
+    similarityPercentage: similarityPercentage
+  };
+}
+
+// new function, it also includes the cases when one of the text input is longer than the other 
+function lcs(text1, text2) {
+  var m = text1.length;
+  var n = text2.length;
+  var output = '';
+  var redWords = [];
+  var orangeWords = [];
+  var blueWords = [];
+
+  // Initialize a 2D array to store the results of subproblems
+  var dp = [];
+  for (var i = 0; i <= m; i++) {
+    dp[i] = [];
+    for (var j = 0; j <= n; j++) {
+      dp[i][j] = -1;
+    }
+  }
+
+  // Function to find the length of LCS using dynamic programming
+  function lcsLength(i, j) {
+    if (i === 0 || j === 0) {
+      return 0;
+    }
+
+    // If the result is already computed, return it
+    if (dp[i][j] !== -1) {
+      return dp[i][j];
+    }
+
+    if (text1[i - 1] === text2[j - 1]) {
+      dp[i][j] = 1 + lcsLength(i - 1, j - 1);
+    } else if (ld(text1[i - 1], text2[j - 1]).similarityPercentage >= 60
+      && ld(text1[i - 1], text2[j - 1]).similarityPercentage < 100) {
+      dp[i][j] = 0.5 + lcsLength(i - 1, j - 1);
+    } else {
+      dp[i][j] = Math.max(lcsLength(i - 1, j), lcsLength(i, j - 1));
+    }
+
+    return dp[i][j];
+  }
+
+  // Function to construct the LCS
+  function constructLCS(i, j) {
+    if (i === 0 && j === 0) {
+      return '';
+    } else if (i === 0) {
+      redWords.push(text2[j - 1]);
+      return '<span class="red">' + text2[j - 1] + '</span> ' + constructLCS(i, j - 1);
+    } else if (j === 0) {
+      orangeWords.push(text1[i - 1]);
+      return '<span class="red orange">' + text1[i - 1] + '</span> ' + constructLCS(i - 1, j);
+    } else if (text1[i - 1] === text2[j - 1]) {
+      return constructLCS(i - 1, j - 1)
+        + '<span>' + text1[i - 1] + '</span> ';
+    } else if (ld(text1[i - 1], text2[j - 1]).similarityPercentage >= 60
+      && ld(text1[i - 1], text2[j - 1]).similarityPercentage < 100) {
+            blueWords.push('<span class="blue">' + text1[i - 1]
+        + '<span class="green">{' + text2[j - 1] + '}</span></span>');
+      return constructLCS(i - 1, j - 1)
+        + '<span class="blue">' + text1[i - 1]
+        + '<span class="green">{'+text2[j - 1] + '}</span></span> ';
+    } else {
+      if (lcsLength(i - 1, j) >= lcsLength(i, j - 1)) {
+        orangeWords.push(text1[i - 1]);
+        return constructLCS(i - 1, j)
+          + '<span class="red orange">' + text1[i - 1] + '</span> ';
+      } else {
+        redWords.push(text2[j - 1]);
+        return constructLCS(i, j - 1)
+          + '<span class="red">' + text2[j - 1] + '</span> ';
+      }
+    }
+  }
+
+  output = constructLCS(m, n);
+
+  return {
+    output: output,
+    redWords: redWords,
+    orangeWords: orangeWords,
+    blueWords: blueWords
+  };
+}
+
+submit.addEventListener('click', function() {
+  rearrangeLayout();
+  submitButtonClicked= true;
+  var word1 = text1.value.trim().split(/\s+/);
+  var word2 = text2.value.trim().split(/\s+/);
+  var wordCount1 = word1.length;
+  var wordCount2 = word2.length;
+  var charCount1 = text1.value.length;
+  var charCount2 = text2.value.length;
+  var charWord1 = Math.round(charCount1 / 5);
+  var charWord2 = Math.round(charCount2 / 5);
+  var L = lcs(word2, word1);
+  var redWords = L.redWords.slice().reverse();
+  var orangeWords = L.orangeWords.slice().reverse();
+  var blueWords = L.blueWords.slice().reverse();
+  var red = redWords.length;
+  var orange = orangeWords.length;
+  var blue = blueWords.length;
+  var fm = red + orange;
+  var error = errorsPercentage(fm, blue, wordCount1);
+
+  //result
+  result.innerHTML = '<b>Total Words: </b>' + wordCount1 + '[~' + charWord1 + ']' + ' words; <br>';
+  result.innerHTML += '<b>Words typed: </b>' + wordCount2 + '[~' + charWord2 + ']' + ' words; <br>';
+  result.innerHTML += '<b>Full Mistakes : </b>' + fm + '; <br>';
+  result.innerHTML += '<b>Half Mistakes : </b>' + blue + '; <br>';
+  result.innerHTML += '<b>Error Percentage: </b>' + error + '%; <br>';
+
+
+  // output
+  output.innerHTML = L.output + '<br>';
+
+  //output2
+  output2.innerHTML = '<b>Half Mistakes:</b><ol>';
+  blueWords.forEach(function(word, index) {
+    output2.innerHTML += (index + 1) + '.' + word + '; ';
+  });
+  output2.innerHTML += '</ol><br>';
+
+  output2.innerHTML += '<b>Missing Words:</b><ol>';
+  redWords.forEach(function(word, index) {
+    output2.innerHTML += (index + 1) + '.<span class="red">' + word + '</span>; ';
+  });
+  output2.innerHTML += '</ol><br>';
+
+  output2.innerHTML += '<b>Extra Words:</b><ol>';
+  orangeWords.forEach(function(word, index) {
+    output2.innerHTML += (index + 1) + '.<span class="red orange">' + word + '</span>; ';
+  });
+  output2.innerHTML += '</ol><br>';
 });
+
+
